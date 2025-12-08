dc1e05cce771eecf290777ba5ab28da9da4d909f
Tarrah <tarrah.elash@gmail.com>
2025-12-08 10:48:01 -0500

    Started HTML linking and JS class creation

---
+++ pages/index/index.js
@@ -0,0 +1,18 @@
+class Board{
+
+    static tileClass = class{
+        constructor(value){
+            this.value - value;
+        }
+
+    };
+
+    constructor(){
+        this.boardObjArray = 4;
+    }
+    genBoard(height,width){
+        console.log('yoMama')
+    }
+}
+
+board = new Board();
