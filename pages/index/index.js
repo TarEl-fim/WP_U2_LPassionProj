3689e5363e83c421bc65578c5b579e994574cb20
Tarrah <tarrah.elash@gmail.com>
2025-12-09 10:49:45 -0500

    Created Board/Tile class and Board generation

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,18 +1,68 @@
 class Board{
 
-    static tileClass = class{
-        constructor(value){
-            this.value - value;
-        }
-
-    };
-
-    constructor(){
-        this.boardObjArray = 4;
+    constructor(h,w){
+        this.Arrayset = this.genBoard(h,w);
+        this.Visualset = this.genTileList(this.Arrayset);
     }
+
+    genTileList(arraySet){
+        //VISUAL DOMs LIST OF OBJs
+        const board = document.createElement('div');
+        board.className = 'gray';
+        board.id = 'gameBoard'
+        for (let h = 0; h < arraySet.length; h++){
+            const invis = document.createElement('div');
+            invis.className = 'invisi';
+            for (let w = 0; w < arraySet[h].length; w++){
+                const eleTile = arraySet[h][w];
+                invis.appendChild(eleTile.element);
+            }
+            board.appendChild(invis);
+        }
+        return board;
+
+    }
+
     genBoard(height,width){
-        console.log('yoMama')
+        //ACTUAL OBJECT LIST FOR EDITING VISUALS
+        const boardList = [];
+        for (let h = 0; h <height; h++){
+            const invisList = [];
+            for (let w = 0; w < width; w++){
+                const tileGen = new tileClass();
+                invisList.push(tileGen);
+            }
+            boardList.push(invisList);
+        }
+        return boardList;
     }
 }
 
-board = new Board();
+class tileClass{
+    constructor(){
+        this.element = this.genTile();
+        this.img = null;
+    }
+
+    genTile(){
+        const tile = document.createElement('div');
+
+        tile.addEventListener('contextmenu', (event) => {
+        console.log('kill yourself');
+        event.preventDefault()
+        })
+
+        tile.className = 'tile';
+        return tile;
+    }  
+
+};
+
+main();
+
+function main(){
+    board = new Board(9,10);
+    const body = document.getElementById('gameArea');
+    body.appendChild(board.Visualset)
+    console.log(board.Visualset);
+}
